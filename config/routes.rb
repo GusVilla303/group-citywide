ad6dead4124c015bee04354fc959587110c8f3eb
GusVilla303 <gusvilla303@gmail.com>
2016-09-26 15:13:14 -0600

    Remove application/hello from routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  get 'application/hello'
-
   root 'application#hello'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
